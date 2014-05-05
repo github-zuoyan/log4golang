0bb46548526405e78adbcbdc0f28c1dd8ec26d3f
C@Z <cz.theng@gmail.com>
2014-05-05 22:48:15 +0800

    init

---
+++ log.go
@@ -0,0 +1,12 @@
+/**
+* A log package for golang
+* 
+* not instead of golang's log but a replenish
+*/
+
+package log
+
+import (
+//	"sync"
+//	"os"
+)
---
+++ test_log.go
@@ -0,0 +1,33 @@
+/**
+* test for log.go
+*/
+package main
+
+import (
+	"fmt"
+	
+)
+
+
+func main(){
+	fmt.Println("Test log4golang")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
