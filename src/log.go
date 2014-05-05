a1de57a72dd450924c4dd68c9cdd70bcb4340711
C@Z <cz.theng@gmail.com>
2014-05-05 23:10:30 +0800

    tick

---
+++ src/log.go
@@ -0,0 +1,18 @@
+/**
+* A log package for golang
+* 
+* not instead of golang's log but a replenish
+*/
+
+package log
+
+import (
+//	"sync"
+//	"os"
+	"fmt"
+)
+
+
+func hello(){
+	fmt.Println("nimei")
+}
---
+++ test/test_log.go
@@ -0,0 +1,35 @@
+/**
+* test for log.go
+*/
+package main
+
+import (
+	"github.com/cz-it/log4golang/log"
+
+	"fmt"
+)
+
+
+func main(){
+	fmt.Println("Test log4golang")
+	log.hello()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
